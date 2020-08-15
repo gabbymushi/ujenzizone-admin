98df5b507a0f3242ad0c0f76af070b082ff7f0d9
Gabriel Patrick <gabrielpatrickmushi@gmail.com>
2020-08-15 16:22:15 +0300

    post user service function

--- src/components/user-profile-lite/UserService.js
+++ src/components/user-profile-lite/UserService.js
@@ -2,9 +2,18 @@ import axios from 'axios';
 
 export const fetchCategories = async () => {
     try {
-        const mafundi = await axios.get('http://localhost:4500/api/v1/categories');
-        return mafundi;
+        const categories = await axios.get('http://localhost:4500/api/v1/categories');
+        return categories;
     } catch (e) {
-        console.log('Hapa', e.message)
+        console.log('Error', e.message)
+    }
+};
+
+export const postUser = async (data) => {
+    try {
+        const user = await axios.post('http://localhost:4500/api/v1/users',data);
+        return user;
+    } catch (e) {
+        console.log('Error', e.message)
     }
 };
